151eb7bb2cc502d338fef25bd79bde9e385f5bd6
bherrschaft <bherrschaft92994@gmail.com>
2024-07-22 15:24:57 -0400

    java!

---
+++ src/HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String friendName = "Mopp";
+        System.out.println("Hi " + friendName + ", how are you?");
+    }
+}
---
+++ src/Vacation.java
@@ -0,0 +1,7 @@
+public class Vacation {
+    public static void main(String[] args) {
+        System.out.println("I would like to visit Italy");
+        System.out.println("I would like to visit Italy");
+        System.out.println("I would like to visit Italy");
+    }
+}
